ccb6a73cdef2573f933a006539d98e632eb88a66
tevonmalone <82242368+tevonmalone@users.noreply.github.com>
2021-04-09 18:43:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,20 +19,33 @@
             $tutorial_title = $_POST['tutorial_title'];
             $tutorial_author = $_POST['tutorial_author'];
             $submission_date = $_POST['submission_date'];
-   
-            $sql = "INSERT INTO tutorials_tbl ".
-               "(tutorial_title,tutorial_author, submission_date) "."VALUES ".
-               "('$tutorial_title','$tutorial_author','$submission_date')";
-               mysql_select_db('defaultdb');
-            $retval = mysql_query( $sql, $conn );
-         
-            if(! $retval ) {
-               die('Could not enter data: ' . mysql_error());
+            
+            mysql_select_db("defaultdb", $conn);
+            $sql="INSERT INTO tutorials_tbl (tutorial_title, tutorial_author, submission_date)
+            VALUES
+            ('$_POST[tutorial_title]','$_POST[tutorial_author]','$_POST[submission_date]')";
+
+            if (!mysql_query($sql,$con))
+            {
+             die('Error: ' . mysql_error());
             }
+            echo "1 record added";
+            mysql_close($conn)
+
+   
+            #$sql = "INSERT INTO tutorials_tbl ".
+            #   "(tutorial_title,tutorial_author, submission_date) "."VALUES ".
+            #   "('$tutorial_title','$tutorial_author','$submission_date')";
+            #   mysql_select_db('defaultdb');
+            #$retval = mysql_query( $sql, $conn );
          
-            echo "Entered data successfully\n";
-            mysql_close($conn);
-         } else {
+            #if(! $retval ) {
+             #  die('Could not enter data: ' . mysql_error());
+            #}
+         
+            #echo "Entered data successfully\n";
+            #mysql_close($conn);
+         #} else {
       ?>
    
       <form method = "post" action = "<?php $_PHP_SELF ?>">
