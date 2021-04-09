5653f84ca38a8f09b1dbd3f0b596e736348857c2
tevonmalone <82242368+tevonmalone@users.noreply.github.com>
2021-04-09 17:27:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,84 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
+<html>
 
-use Cowsayphp\Farm;
+   <head>
+      <title>Add New Record in MySQL Database</title>
+   </head>
 
-header('Content-Type: text/plain');
+   <body>
+      <?php
+         if(isset($_POST['add'])) {
+            $dbhost = 'db-mysql-mtg-14981-do-user-9057563-0.b.db.ondigitalocean.com:25060';
+            $dbuser = 'doadmin';
+            $dbpass = 'vmfgowz9s3x40idw';
+            $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+         
+            if(! $conn ) {
+               die('Could not connect: ' . mysql_error());
+            }
 
-$text = "Tevon Built an App on Digital Ocean!";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
+            if(! get_magic_quotes_gpc() ) {
+               $tutorial_title = addslashes ($_POST['tutorial_title']);
+               $tutorial_author = addslashes ($_POST['tutorial_author']);
+            } else {
+               $tutorial_title = $_POST['tutorial_title'];
+               $tutorial_author = $_POST['tutorial_author'];
+            }
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+            $submission_date = $_POST['submission_date'];
+   
+            $sql = "INSERT INTO tutorials_tbl ".
+               "(tutorial_title,tutorial_author, submission_date) "."VALUES ".
+               "('$tutorial_title','$tutorial_author','$submission_date')";
+               mysql_select_db('TUTORIALS');
+            $retval = mysql_query( $sql, $conn );
+         
+            if(! $retval ) {
+               die('Could not enter data: ' . mysql_error());
+            }
+         
+            echo "Entered data successfully\n";
+            mysql_close($conn);
+         } else {
+      ?>
+   
+      <form method = "post" action = "<?php $_PHP_SELF ?>">
+         <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
+            <tr>
+               <td width = "250">Tutorial Title</td>
+               <td>
+                  <input name = "tutorial_title" type = "text" id = "tutorial_title">
+               </td>
+            </tr>
+         
+            <tr>
+               <td width = "250">Tutorial Author</td>
+               <td>
+                  <input name = "tutorial_author" type = "text" id = "tutorial_author">
+               </td>
+            </tr>
+         
+            <tr>
+               <td width = "250">Submission Date [   yyyy-mm-dd ]</td>
+               <td>
+                  <input name = "submission_date" type = "text" id = "submission_date">
+               </td>
+            </tr>
+      
+            <tr>
+               <td width = "250"> </td>
+               <td> </td>
+            </tr>
+         
+            <tr>
+               <td width = "250"> </td>
+               <td>
+                  <input name = "add" type = "submit" id = "add"  value = "Add Tutorial">
+               </td>
+            </tr>
+         </table>
+      </form>
+   <?php
+      }
+   ?>
+   </body>
+</html>
