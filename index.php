aa142b35457d787ecbfad636f1f2558ea4c51956
tevonmalone <82242368+tevonmalone@users.noreply.github.com>
2021-04-09 18:55:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,29 +15,23 @@
             if(! $conn ) {
                die('Could not connect: ' . mysql_error());
             }
-
-            if(! get_magic_quotes_gpc() ) {
-               $tutorial_title = addslashes ($_POST['tutorial_title']);
-               $tutorial_author = addslashes ($_POST['tutorial_author']);
-            } else {
-               $tutorial_title = $_POST['tutorial_title'];
-               $tutorial_author = $_POST['tutorial_author'];
-            }
-
+ 
+            $tutorial_title = $_POST['tutorial_title'];
+            $tutorial_author = $_POST['tutorial_author'];
             $submission_date = $_POST['submission_date'];
-   
-            $sql = "INSERT INTO tutorials_tbl ".
-               "(tutorial_title,tutorial_author, submission_date) "."VALUES ".
-               "('$tutorial_title','$tutorial_author','$submission_date')";
-               mysql_select_db('TUTORIALS');
-            $retval = mysql_query( $sql, $conn );
-         
-            if(! $retval ) {
-               die('Could not enter data: ' . mysql_error());
+            
+            mysql_select_db("defaultdb", $conn);
+            $sql="INSERT INTO tutorials_tbl (tutorial_title, tutorial_author, submission_date)
+            VALUES
+            ('$_POST[tutorial_title]','$_POST[tutorial_author]','$_POST[submission_date]')";
+
+            if (!mysql_query($sql,$con))
+            {
+             die('Error: ' . mysql_error());
             }
-         
-            echo "Entered data successfully\n";
-            mysql_close($conn);
+            echo "1 record added";
+            mysql_close($conn)         
+            
          } else {
       ?>
    
