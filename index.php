05f37af3407d36da08f75375d267600d8d5c02ac
tevonmalone <82242368+tevonmalone@users.noreply.github.com>
2021-04-09 17:17:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
+$text = "Tevon Built an App on Digital Ocean!";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
